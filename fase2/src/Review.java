8f0d77cbb0e50635143f3ad373a0dccf02971d93
Anaa10 <a93284@alunos.uminho.pt>
2022-01-20 16:42:30 +0000

    class Review

--- fase2/src/Review.java
+++ fase2/src/Review.java
@@ -1,2 +1,31 @@
 public class Review {
+
+    private String userId;
+    private String placeId;
+    private float classification;
+    private String comment;
+
+    public Review(String userId, String placeId, float classification, String comment){
+        this.userId = userId;
+        this.placeId = placeId;
+        this.classification = classification;
+        this.comment = comment;
+    }
+
+    public Review(Review rev){
+        this.userId = rev.getUserId();
+        this.placeId = rev.getPlaceId();
+        this.classification = rev.getClassification();
+        this.comment = rev.getComment();
+    }
+
+    public String getUserId(){ return this.userId; }
+
+    public String getPlaceId(){ return this.placeId; }
+
+    public float getClassification(){ return this.classification; }
+
+    public String getComment(){ return this.comment; }
+
+    public Review clone(){ return new Review(this);}
 }
